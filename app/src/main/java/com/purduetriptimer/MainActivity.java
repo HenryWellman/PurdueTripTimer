3a3fffb80ecac7dd88a70130444668461184fa87
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2020-10-17 12:19:59 -0400

    purdue building array

--- app/src/main/java/com/purduetriptimer/MainActivity.java
+++ app/src/main/java/com/purduetriptimer/MainActivity.java
@@ -1,9 +1,41 @@
 package com.purduetriptimer;
 
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String[] PURDUE_BUILDINGS = new String[] {"ADPA-C - Aspire at Discovery Park",
+            "CARY - Cary Quadrangle", "ERHT - Earhart Residence Hall", "FST - First Street Towers",
+            "GRFN - Griffin Residence Hall North", "GRFS - Griffin Residence Hall South (Formerly Third Street Suites)",
+            "HARR - Harrison Residence Hall", "HAWK - Hawkins Hall", "HCRN - Honors College and Residences North",
+            "HCRS - Honors College and Residences South", "HILL - Hillenbrand Residence Hall",
+            "HLTP - Hilltop Apartments", "MCUT - McCutcheon Residence Hall", "MRDH - Meredith Residence Hall",
+            "MRDS - Meredith Residence Hall South", "OWEN - Owen Residence Hall", "PVIL - Purdue Village",
+            "SHRV - Shreve Residence Hall", "TARK - Tarkington Residence Hall", "WILY - Wiley Residence Hall",
+            "WNSR - Windsor Residence Halls", "FORD - Ford Dining Court", "WDCT - Wiley Dining Court",
+            "AERO - Aerospace Science Laboratory", "ARMS - Armstrong Hall of Engineering",
+            "BCHM - Biochemistry Building", "BIND - Bindley Bioscience Center",
+            "BRNG - Beering Hall of Liberal Arts and Education", "BRWN - Brown Laboratory of Chemistry",
+            "CL50 - Class of 1950 Lecture Hall", "CRTN - Hobart and Russell Creighton Hall of Animal Sciences",
+            "EE - Electrical Engineering Building", "EHSB - Equine Health Sciences Building",
+            "ELLT - Elliott Hall of Music", "FRNY - Forney Hall of Chemical Engineering", "HAAS - Haas Hall",
+            "HAMP - Hampton Hall of Civil Engineering", "HANS - Hansen Life Sciences Research Building",
+            "HIKS - Hicks Undergraduate Library", "HLAB - Herrick Laboratories", "JNSN - Johnson Hall of Nursing",
+            "KNOY - Knoy Hall of Technology", "KRCH - Krach Leadership Center", "LILY - Lilly Hall of Life Sciences",
+            "LWSN - Lawson Computer Science Building", "LYNN - Lynn Hall of Veterinary Medicine",
+            "MATH - Mathematical Sciences Building", "ME - Mechanical Engineering Building",
+            "MJIS - Jischke Hall of Biomedical Engineering", "MRRT - Marriott Hall", "MTHW - Matthews Hall",
+            "PHYS - Physics Building", "REC - Recitation Building", "RHPH - Heine Pharmacy Building",
+            "SMTH - Smith Hall", "STON - Stone Hall", "UNIV - University Hall", "WALC - Wilmeth Active Learning Center",
+            "YONG - Young Hall", "HOVD  - Hovde Hall of Administration",
+            "PUSH - Purdue University Student Health Center", "SCHL - Schleman Hall of Student Services",
+            "SMLY - Smalley Center for Housing and Food Services Administration", "AQUA - Boilermaker Aquatic Center",
+            "CREC - Co-Rec", "MACK - Mackey Arena", "PMU - Purdue Memorial Union", "STDM - Ross-Ade Stadium",
+            "STEW - Stewart Center", "TREC - Turf Recreation Exercise Center"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
