5b42045de2f0db3054c9ac6fdde05dc4dccd6270
aaryan-gautam <aaryanyo123@gmail.com>
2020-10-17 23:20:30 +0300

    made "travel" method a dropdown

--- app/src/main/java/com/purduetriptimer/MainActivity.java
+++ app/src/main/java/com/purduetriptimer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,33 +12,34 @@ public class MainActivity extends AppCompatActivity {
             "Aspire at Discovery Park", "Cary Quadrangle", "Griffin Residence Hall North",
             "Griffin Residence Hall South (Formerly Third Street Suites)"
     };
-    private static final String[] transportationMethod = new String[] {
-            "Walking", "Bicycle", "Skateboard"
+
+    private static final String[] modeOfTransportations = new String[]{
+            "Walking", "Bicycle","Skateboard", "E-scooter"
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        //Travel from
+        //Travel from with autocompletion
         AutoCompleteTextView fromText = findViewById(R.id.travelFrom);
         ArrayAdapter<String> fromAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, places);
         fromText.setAdapter(fromAdapter);
 
-        //Travel to
+        //Travel to with autocompletion
         AutoCompleteTextView toText = findViewById(R.id.travelTo);
         ArrayAdapter<String> toAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, places);
         toText.setAdapter(toAdapter);
 
-        //Travel method
-        AutoCompleteTextView travelText = findViewById(R.id.travelMethod);
-        ArrayAdapter<String> travelAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, transportationMethod);
-        travelText.setAdapter(travelAdapter);
-
+        //Travel method with a spinner
+        Spinner dropdown = findViewById(R.id.travelMethod);
+        ArrayAdapter<String> dropdownAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, modeOfTransportations);
+        dropdown.setAdapter(dropdownAdapter);
     }
 
 
